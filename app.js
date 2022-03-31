4953bafd97eb1199e76a6c08732cbce4e05ea6c1
PabloValles <valles.pablo@hotmail.com>
2022-03-31 12:54:03 -0300

    (new) Funcionalidad con MVC

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const routerMain = require("./routers/main");
+const path = require("path");
+
 const app = express();
+const publicPath = path.resolve(__dirname, "./public");
 
 app.listen(3000, () => console.log("Server runing in port 3000"));
+
+app.use(express.static(publicPath));
+
+app.use("/", routerMain);
