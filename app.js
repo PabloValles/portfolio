91a7a149f1eb0870f8749ebacddd92dbafc09294
PabloValles <valles.pablo@hotmail.com>
2022-04-04 15:01:37 -0300

    (new) ejs - y render para las vistas

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+
 const path = require("path");
 const routerMain = require("./routers/main");
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, "./public");
 
-app.listen(PORT, () => console.log(`Server runing in port ${PORT}`));
+app.set("view engine", "ejs");
 
 app.use(express.static(publicPath));
+
+app.listen(PORT, () => console.log(`Server runing in port ${PORT}`));
 
 app.use("/", routerMain);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,6 @@
-const path = require("path");
-
 let mainController = {
-  index: (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../views/home.html")),
-  about: (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../views/about.html")),
+  index: (req, res) => res.render("home"),
+  about: (req, res) => res.render("about"),
 };
 
 module.exports = mainController;
